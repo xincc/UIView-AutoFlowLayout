90bebc1ec15f39e7a7380d620b83bb811c6d5eb0
xincc <willxincc@163.com>
2015-06-04 18:11:42 +0800

    lisence

--- lib/UIView+AutoLayout.m
+++ lib/UIView+AutoLayout.m
@@ -1,10 +1,25 @@
+// UIView+AutoLayout.m
+// Copyright (c) 2014–2015 xincc (https://github.com/xincc)
 //
-//  UIView+AutoLayout.m
-//  zhaopin
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
-//  Created by xincc on 11/5/14.
-//  Copyright (c) 2015 __my_company. All rights reserved.
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
 //
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+
 #import "UIView+AutoLayout.h"
 
 ALMargin ALMarginMake(CGFloat l,CGFloat r, CGFloat t, CGFloat b){
@@ -65,7 +80,7 @@ UIKIT_STATIC_INLINE void cleanRemoveFromSuperview(UIView * view ) {
 - (void)removeView:(UIView*)view{
     
     cleanRemoveFromSuperview(view);
-    if ([self.subviews containsObject:view]) {
+    if (view.superview == self) {
         [view removeFromSuperview];
     }
 }
