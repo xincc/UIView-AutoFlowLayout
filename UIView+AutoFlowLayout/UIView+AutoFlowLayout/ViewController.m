7c2d3b862ee01841113baf986ce3f6495898cbb9
xincc <willxincc@163.com>
2015-06-04 17:59:52 +0800

    make demo

--- UIView+AutoFlowLayout/UIView+AutoFlowLayout/ViewController.m
+++ UIView+AutoFlowLayout/UIView+AutoFlowLayout/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "UIView+AutoLayout.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,48 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //bg view
+    UIView * viewBackGround = [[UIView alloc] initWithFrame:CGRectZero];
+    viewBackGround.backgroundColor = UIColor.redColor;
+    [self.view addSubview:viewBackGround fillByMargin:ALMarginZero()];
+
+    
+    //clip to two part
+    
+    UIView * topPart = [[UIView alloc] initWithFrame:CGRectZero];
+    UIView * bottomPart = [[UIView alloc] initWithFrame:CGRectZero];
+    topPart.backgroundColor = UIColor.blackColor;
+    bottomPart.backgroundColor = UIColor.darkGrayColor;
+    
+    [viewBackGround addSubviews:@[topPart, bottomPart] flowLayoutDirection:ALLayoutDirectionVertical fillByMargin:ALMarginMakeLeft(50)];
+    
+    //set top part
+    UIView * topPart1 = [[UIView alloc] initWithFrame:CGRectZero];
+    UIView * topPart2 = [[UIView alloc] initWithFrame:CGRectZero];
+    UIView * topPart3 = [[UIView alloc] initWithFrame:CGRectZero];
+    topPart1.backgroundColor = UIColor.brownColor;
+    topPart2.backgroundColor = UIColor.greenColor;
+    topPart3.backgroundColor = UIColor.blueColor;
+
+    [topPart addSubviews:@[topPart1,topPart2,topPart3] flowLayoutDirection:ALLayoutDirectionHorizontal fillByMargin:ALMarginMakeTop(50)];
+    
+    //set bottom part
+    UIView * bottomPart1 = [[UIView alloc] initWithFrame:CGRectZero];
+    UIView * bottomPart2 = [[UIView alloc] initWithFrame:CGRectZero];
+    UIView * bottomPart3 = [[UIView alloc] initWithFrame:CGRectZero];
+    bottomPart1.backgroundColor = UIColor.cyanColor;
+    bottomPart2.backgroundColor = UIColor.yellowColor;
+    bottomPart3.backgroundColor = UIColor.purpleColor;
+
+    [bottomPart addSubviews:@[bottomPart1,bottomPart2,bottomPart3] flowLayoutDirection:ALLayoutDirectionVertical fillByMargin:ALMarginMakeRight(50)];
+
+    
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
 }
 
 - (void)didReceiveMemoryWarning {
